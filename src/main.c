22cde81e6b8e51e0e5e3d91fd1c2244de82fa290
hrithik098 <hrithiky328@gmail.com>
2020-06-16 11:55:19 +0530

    Added averagin and min-max function with dynamic memalloc

--- src/main.c
+++ src/main.c
@@ -1,11 +1,10 @@
 #include "mgos.h"
 #include "mgos_i2c.h"
 #include "mgos_imu.h"
+#include "common/mbuf.h"
 
-int count = 0, check = 0;
-float imu_x[5], imu_y[5], imu_z[5];
-float sum_x, sum_y, sum_z;   //
-float avg_x, avg_y, avg_z;
+float *imuptr;
+int count = 0;
 
 /*! \mainpage IMU testing
  *
@@ -15,67 +14,41 @@ float avg_x, avg_y, avg_z;
  *
  */
 
-
-
-void get_average_value(float ax, float ay, float az)
+float average_me(float reading, int n, float *ptr)
 {
 
-  if (count == 3)
+  float average = 1, sum = 0, max = 0, min = reading;
+  ptr = malloc(n * sizeof(int));
+
+  // if memory cannot be allocated
+  if (ptr == NULL)
+  {
+    LOG(LL_ERROR, ("Error! memory not allocated."));
+    exit(0);
+  }
+  *(ptr + count) = reading;
+  count++;
+  if (count == n)
   {
     count = 0;
-    imu_x[count] = ax;
-    imu_y[count] = ay;
-    imu_z[count] = az;
-    count += 1;
-  }
-  else
-  {
-    imu_x[count] = ax;
-    imu_y[count] = ay;
-    imu_z[count] = az;
-    count += 1;
-  }
-  check += 1;
-  if (check > 2)
-  {
-    for (int num = 0; num < 3; num++)
+    for (int i = 0; i < n; ++i)
     {
-      sum_x = sum_x + imu_x[num];
-      sum_y = sum_y + imu_y[num];
-      sum_z = sum_z + imu_z[num];
+      sum += *(ptr + i);
+      if (max < *(ptr + i))
+            max = *(ptr + i);
+      if (min < *(ptr + i))
+            min = *(ptr + i);
+      
     }
+    average = sum / n;
   }
-  // storing the largest number in [3] index
-  for (int i = 1; i < 3; ++i) {
-        if (imu_x[3] < imu_x[i])
-            imu_x[3] = imu_x[i];
-        if (imu_y[3] < imu_y[i])
-            imu_y[3] = imu_y[i];
-        if (imu_z[3] < imu_z[i])
-            imu_z[3] = imu_z[i];
-    }
-    // reference value to find smallest reading 
-  imu_x[4]=imu_x[0];
-  imu_y[4]=imu_y[0];
-  imu_z[4]=imu_z[0];
-  // Storing the smallest number in [4] index
-  for (int i = 1; i < 3; ++i) {
-        if (imu_x[4] > imu_x[i])
-            imu_x[4] = imu_x[i];
-        if (imu_y[4] > imu_y[i])
-            imu_y[4] = imu_y[i];
-        if (imu_z[4] > imu_z[i])
-            imu_z[4] = imu_z[i];
-    }
-  avg_x = sum_x / 3;
-  avg_y = sum_y / 3;
-  avg_z = sum_z / 3;
-  LOG(LL_INFO, ("TCU: Acceleration average value X=%.2f Y=%.2f Z=%.2f", avg_x, avg_y, avg_z));
+
+  // deallocating the memory
+  free(ptr);
+
+  return average;
 }
-/**< Get average values of acceleration in ax, ay and az. Store 3 latest values in buffer and  everytime new values come and last values gets pushed out of buffer.*/
 
-
- 
 void get_imu_reading_cb(void *user_data)
 {
 
@@ -88,10 +61,11 @@ void get_imu_reading_cb(void *user_data)
 
   if (mgos_imu_accelerometer_get(imu, &ax, &ay, &az))
     LOG(LL_INFO, ("TCU: type=%-10s Accel X=%.2f Y=%.2f Z=%.2f", mgos_imu_accelerometer_get_name(imu), ax, ay, az));
-  get_average_value(ax, ay, az);
+  ax = average_me(ax, 10, imuptr);
+  ay = average_me(ay, 10, imuptr);
+  az = average_me(az, 10, imuptr);
 }
 /**< Get acceleration values */
-
 
 enum mgos_app_init_result mgos_app_init(void)
 {
@@ -118,7 +92,6 @@ enum mgos_app_init_result mgos_app_init(void)
   acc_opts.odr = 100;    // Hz
   if (!mgos_imu_accelerometer_create_i2c(imu, i2c, 0x68, &acc_opts))
     LOG(LL_ERROR, ("Cannot create accelerometer on IMU"));
-
   mgos_set_timer(1000, true, get_imu_reading_cb, imu);
   return true;
 }
